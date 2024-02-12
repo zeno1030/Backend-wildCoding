13d4e07e461141332d26668d4bd5c40677780172
ZENO <zeno1030@naver.com>
2024-02-12 17:47:41 +0900

    코드 정리

--- src/test/java/practice/board/article/service/ArticleServiceTest.java
+++ src/test/java/practice/board/article/service/ArticleServiceTest.java
@@ -1,7 +1,5 @@
 package practice.board.article.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
