c543b063fb5524e2da9f09eec775791595e67a21
ZENO <zeno1030@naver.com>
2024-02-24 22:42:33 +0900

    테스트수정필요

--- src/test/java/practice/board/article/service/ArticleServiceTest.java
+++ src/test/java/practice/board/article/service/ArticleServiceTest.java
@@ -1,13 +1,18 @@
 package practice.board.article.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ArticleServiceTest {
+	@Autowired
+	private ArticleService articleService;
+
 	@Test
 	void findArticle() {
 		//given
+		
 		//when
 		//then
 	}
